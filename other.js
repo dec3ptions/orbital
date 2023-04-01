4e9b5f83300fab2c96e69cb5814e2d3552cade78
dec3ptions <105021570+dec3ptions@users.noreply.github.com>
2023-04-01 22:18:59 +0100

    v2.0.2

---
+++ other.js
@@ -0,0 +1,4 @@
+var dailyGift_amt = Number(prompt("How many daiy gifts do you want to open?"));
+for (let i = 0; i < dailyGift_amt; i++) {
+    daily_gift.open();console.clear();console.log('Opened!');
+};
