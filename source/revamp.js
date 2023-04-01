63ae6dbf2deb09e0b04bf295167bf89561396aed
dec3ptions <105021570+dec3ptions@users.noreply.github.com>
2023-04-01 21:26:58 +0100

    v2.0.0

--- source/revamp.js
+++ source/revamp.js
@@ -1,20 +1,16 @@
-// Config
-fetch('https://raw.githubusercontent.com/dec3ptions/orbital/hacks/config.json')
-  .then(response => response.json())
-  .then(data => config2 = data)
-  .catch(error => console.error(error));
-
 // Variables
+const getConfig = async (url) => { const response = await fetch(url); const data = await response.json(); return data; }; const dec3ptions_config = await getConfig("https://raw.githubusercontent.com/dec3ptions/orbital/hacks/config.json");
+
 const dec3ptions_blueprint = [
   
-  { "id": "dec3ptions_1_specific", "source": "https://raw.githubusercontent.com/dec3ptixns/orb/main/functions/dec3ptions_1_specific.js", "function": function() { alert("This has not been programmed yet."); } },
-  { "id": "dec3ptions_1_rapidSpecific", "source": "https://raw.githubusercontent.com/dec3ptixns/orb/main/functions/dec3ptions_1_rapidSpecific.js", "function": function() { alert("This has not been programmed yet."); } },
+  { "id": "dec3ptions_1_specific", "source": "https://raw.githubusercontent.com/dec3ptixns/orb/main/functions/dec3ptions_1_specific.js", "function": async function() { var weapon_id_choice = String(prompt("Which weapon, in the weapon list, do you want to get?")); var weapon_amt_coice = Number(prompt("How many do you want to get?")); var n = await sync.async_get("w_"+weapon_id_choice) || 0; sync.async_set("w_"+weapon_id_choice, n+weapon_amt_coice); dagger_selection.init() } },
+  { "id": "dec3ptions_1_rapidSpecific", "source": "https://raw.githubusercontent.com/dec3ptixns/orb/main/functions/dec3ptions_1_rapidSpecific.js", "function": async function() { var rapid_weapon_id_choice = String(prompt("Which weapon, in the weapon list, do you want to get?")); setInterval(async () => { var n = await sync.async_get("w_"+rapid_weapon_id_choice) || 0; await sync.async_set("w_"+rapid_weapon_id_choice, n+1); await dagger_selection.init(); }, 10) } },
   { "id": "dec3ptions_1_levelbased", "source": "https://raw.githubusercontent.com/dec3ptixns/orb/main/functions/dec3ptions_1_levelbased.js", "function": function() { dagger_selection.reward_level_clear();console.clear();console.log('Opened!'); } },
   { "id": "dec3ptions_1_rapidLevelbased", "source": "https://raw.githubusercontent.com/dec3ptixns/orb/main/functions/dec3ptions_1_rapidLevelbased.js", "function": function() { setInterval(function dailyRewards() {dagger_selection.reward_level_clear();console.clear();console.log('Opened!');}, 20); } },
 
   { "id": "dec3ptions_2_dagger", "source": "https://raw.githubusercontent.com/dec3ptixns/orb/main/functions/dec3ptions_2_dagger.js", "function": function() { var option_equipDaggers_equipDagger = "";let choice_EQUIP_DAGGERS_DAGGER_weapon_id = prompt("Which weapon (in weaponList) to equip.", option_equipDaggers_equipDagger);if (choice_EQUIP_DAGGERS_DAGGER_weapon_id == null || choice_EQUIP_DAGGERS_DAGGER_weapon_id == "") {return} else {option_equipDaggers_equipDagger = choice_EQUIP_DAGGERS_DAGGER_weapon_id};dagger_selection.equip_dagger(option_equipDaggers_equipDagger); } },
   { "id": "dec3ptions_2_loadout", "source": "https://raw.githubusercontent.com/dec3ptixns/orb/main/functions/dec3ptions_2_loadout.js", "function": function() { var option_equipDaggers_equipLoadout = "";let choice_EQUIP_DAGGERS_LOADOUT_weapon_id = prompt("Which weapon (in weaponList) to equip 4 times.", option_equipDaggers_equipLoadout);if (choice_EQUIP_DAGGERS_LOADOUT_weapon_id == null || choice_EQUIP_DAGGERS_LOADOUT_weapon_id == "") {return} else {option_equipDaggers_equipLoadout = choice_EQUIP_DAGGERS_LOADOUT_weapon_id};dagger_selection.equip_dagger(option_equipDaggers_equipLoadout);dagger_selection.equip_dagger(option_equipDaggers_equipLoadout);dagger_selection.equip_dagger(option_equipDaggers_equipLoadout);dagger_selection.equip_dagger(option_equipDaggers_equipLoadout); } },
-  { "id": "dec3ptions_2_random", "source": "https://raw.githubusercontent.com/dec3ptixns/orb/main/functions/dec3ptions_2_random.js", "function": function() { alert("This has not been programmed yet."); } },
+  { "id": "dec3ptions_2_random", "source": "https://raw.githubusercontent.com/dec3ptixns/orb/main/functions/dec3ptions_2_random.js", "function": function() { var int = Math.floor(Math.random() * 33) + 1; const daggers = [{"id":"starter_sword"},{"id":"spear1"},{"id":"kitchen_knife"},{"id":"spear2"},{"id":"bullet"},{"id":"banana"},{"id":"short_dagger"},{"id":"katana"},{"id":"cool1"},{"id":"pixel1"},{"id":"diaknife"},{"id":"carrotknife"},{"id":"axe"},{"id":"battle_axe"},{"id":"claw"},{"id":"eye_piercer"},{"id":"long_axe"},{"id":"mace"},{"id":"rocket"},{"id":"staff"},{"id":"wolverine"},{"id":"longsword"},{"id":"broom"},{"id":"speedsword"},{"id":"shield"},{"id":"pendulum"},{"id":"slow_pendulum"},{"id":"long_pendulum"},{"id":"butterfly"},{"id":"lol_fist"},{"id":"superfast"},{"id":"tiny_fast_dagger"},{"id":"wearable"}]; dagger_selection.equip_dagger(daggers[int].id); dagger_selection.init() } },
 
   { "id": "dec3ptions_3_infinite", "source": "https://raw.githubusercontent.com/dec3ptixns/orb/main/functions/dec3ptions_3_infinite.js", "function": function() { setInterval(function infUpgrades() {upgrades.damage = 999;upgrades.health = 999;upgrades.speed = 99;upgrades.spin = 999}, 250); } },
   { "id": "dec3ptions_3_free", "source": "https://raw.githubusercontent.com/dec3ptixns/orb/main/functions/dec3ptions_3_free.js", "function": function() { upgrade_blueprint['damage'].costs = [0, 0, 0, 0, 0, 0, 0, 0];upgrade_blueprint['health'].costs = [0, 0, 0, 0, 0, 0, 0, 0];upgrade_blueprint['speed'].costs = [0, 0, 0, 0, 0, 0, 0, 0];upgrade_blueprint['spin'].costs = [0, 0, 0, 0, 0, 0, 0, 0]; upgrades.init(); } },
@@ -24,7 +20,7 @@ const dec3ptions_blueprint = [
 
   { "id": "dec3ptions_5_instant", "source": "https://raw.githubusercontent.com/dec3ptixns/orb/main/functions/dec3ptions_5_instant.js", "function": function() { daily_gift.open(); } },
   { "id": "dec3ptions_5_infinite", "source": "https://raw.githubusercontent.com/dec3ptixns/orb/main/functions/dec3ptions_5_infinite.js", "function": function() { setInterval(function dailyRewards() {daily_gift.open();console.clear();console.log('Opened!')}, 10); } },
-  //{ "id": "dec3ptions_5_custom", "source": "https://raw.githubusercontent.com/dec3ptixns/orb/main/functions/dec3ptions_5_custom.js", "function": function() {  } },
+  { "id": "dec3ptions_5_custom", "source": "https://raw.githubusercontent.com/dec3ptixns/orb/main/functions/dec3ptions_5_custom.js", "function": function() { alert("This has not been programmed in yet."); } },
 
   { "id": "dec3ptions_6_play", "source": "https://raw.githubusercontent.com/dec3ptixns/orb/main/functions/dec3ptions_6_play.js", "function": function() { var option_levels_play = "";let choice_LEVELS_play = prompt("Which level do you want to play?", option_levels_play);if (choice_LEVELS_play == null || choice_LEVELS_play == "") {return} else {option_levels_play = [choice_LEVELS_play]};state.set("playing", {map_id: map_info[option_levels_play - 1].id}); } },
 
@@ -65,8 +61,11 @@ document.getElementById('screen_dec3ptions').insertAdjacentHTML('beforeEnd',
     '<div class="button" id="dec3ptions_5_infinite" >Infinite</div>' +
     '<div class="button" id="dec3ptions_5_custom" >Custom</div>' +
 
-  '<h1>LEVELS<h1>' +
+    '<h1>LEVELS<h1>' +
     '<div class="button" id="dec3ptions_6_play" >Play</div>' +
+
+    '<h1>OTHER / INFO<h1>' +
+    '<div class="button" id="dec3ptions_7_weaponlist" >Weapon list</div>' +
 
 '</div>');
 
